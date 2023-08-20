c78d553eed710023d0bc066c33dc26b00f59ed02
omarjafor <omarjaforchy@gmail.com>
2023-08-21 00:10:20 +0600

    primary done

--- js/script.js
+++ js/script.js
@@ -26,10 +26,10 @@ function addToTotalPrice(price) {
     if (total >= 200) {
         applyButton.removeAttribute('disabled');
         makePurchaseButton.removeAttribute('disabled')
-        document.getElementById('totalBalance').innerText = total;
+        document.getElementById('totalBalance').innerText = total.toFixed(2);
     } else if (total > 0) {
         makePurchaseButton.removeAttribute('disabled')
-        document.getElementById('totalBalance').innerText = total;
+        document.getElementById('totalBalance').innerText = total.toFixed(2);
     }
     else {
         applyButton.setAttribute('disabled', true)
@@ -37,28 +37,84 @@ function addToTotalPrice(price) {
     }
 }
 
-document.getElementById('couponBox').addEventListener('keyup', function (event) {
-    const text = event.target.value;
-    if (total >= 200 && text === 'SELL200') {
-        applyButton.removeAttribute('disabled');
-        applyButton.addEventListener('click', function (e) {
-            const discountPrice = parseFloat(total*20/100);
-            document.getElementById('discountPrice').innerText = discountPrice.toFixed(2);
-            const totalBalance = total - discountPrice ;
-            document.getElementById('totalBalance').innerText = totalBalance.toFixed(2);
-            console.log(total, discountPrice, totalBalance);
-        })
+applyButton.addEventListener('click', function () {
+    const text = document.getElementById('couponBox').value;
+    if (text === 'SELL200') {
+        const discountPrice = parseFloat(total * 20 / 100);
+        document.getElementById('discountPrice').innerText = discountPrice.toFixed(2);
+        const totalBalance = total - discountPrice;
+        document.getElementById('totalBalance').innerText = totalBalance.toFixed(2);
     }
 });
 
 function refresh() {
-    location.reload(location.href = "#");
+    document.getElementById('couponBox').value = '';
+    document.getElementById('entrylist').childNodes.remove();
+    document.getElementById('totalPrice').innerText = '00.00';
+    document.getElementById('totalBalance').innerText = '00.00';
+    document.getElementById('discountPrice').innerText = '00.00';
 };
 
 // Calling Function On click ........................
 function card1() {
     const name = getName('card1Name');
     const price = getPrice('card1Price');
+    setName(name);
+    addToTotalPrice(price);
+}
+
+function card2() {
+    const name = getName('card2Name');
+    const price = getPrice('card2Price');
+    setName(name);
+    addToTotalPrice(price);
+}
+
+function card3() {
+    const name = getName('card3Name');
+    const price = getPrice('card3Price');
+    setName(name);
+    addToTotalPrice(price);
+}
+
+function card4() {
+    const name = getName('card4Name');
+    const price = getPrice('card4Price');
+    setName(name);
+    addToTotalPrice(price);
+}
+
+function card5() {
+    const name = getName('card5Name');
+    const price = getPrice('card5Price');
+    setName(name);
+    addToTotalPrice(price);
+}
+
+function card6() {
+    const name = getName('card6Name');
+    const price = getPrice('card6Price');
+    setName(name);
+    addToTotalPrice(price);
+}
+
+function card7() {
+    const name = getName('card7Name');
+    const price = getPrice('card7Price');
+    setName(name);
+    addToTotalPrice(price);
+}
+
+function card8() {
+    const name = getName('card8Name');
+    const price = getPrice('card8Price');
+    setName(name);
+    addToTotalPrice(price);
+}
+
+function card9() {
+    const name = getName('card9Name');
+    const price = getPrice('card9Price');
     setName(name);
     addToTotalPrice(price);
 }
